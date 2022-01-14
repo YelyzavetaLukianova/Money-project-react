5ff2c0c894399afdb3e505f3c5175aeca9a87211
YelyzavetaLukianova <yelyzaveta.lukianova@gmail.com>
2022-01-14 14:00:44 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import { Component } from 'react';
-import './App.css';
-import Phonebook from './components/Phonebook/Phonebook';
-// import Statistics from './components/Statistics';
-// import FriendList from './components/FriendList';
-// import TransactionHistory from './components/TransactionHistory';
-// import data from './data/data';
-// import transactions from './data/transactions';
-
 function App() {
-  // state = {
-  //   contacts: [],
-  //   name: '',
-  // };
-
   return (
     <>
       <h1>hello</h1>
-      <Phonebook />
     </>
   );
 }

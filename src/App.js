be83e24bd438d90ac547da18d8ee5a7b9672a249
YelyzavetaLukianova <yelyzaveta.lukianova@gmail.com>
2022-01-14 13:35:38 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+import './App.css';
+import Phonebook from './components/Phonebook/Phonebook';
+// import Statistics from './components/Statistics';
+// import FriendList from './components/FriendList';
+// import TransactionHistory from './components/TransactionHistory';
+// import data from './data/data';
+// import transactions from './data/transactions';
+
+function App() {
+  // state = {
+  //   contacts: [],
+  //   name: '',
+  // };
+
+  return (
+    <>
+      <h1>hello</h1>
+      <Phonebook />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Phonebook.css';
+
+class Phonebook extends React.Component {
+  render() {
+    return <h1>Phonebook</h1>;
+  }
+}
+
+export default Phonebook;
